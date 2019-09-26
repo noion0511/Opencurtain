5f4ed5f47f2379dc5c307f98a1bbc2442426f548
noion <noion0511@gmail.com>
2019-09-26 19:18:46 +0900

    모델추가

--- restserver/models.py
+++ restserver/models.py
@@ -1,3 +1,68 @@
+import datetime
+from django.db import models
+#사용자, 구독, 게시판, 학교, 학부, 학과, 게시물, 댓글
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+   #def setup(self):
+   #   self.user = get_user_model().objects.create_user(
+   #      username="username",
+   #      email="email"
+   #      password="password",
+   username_text = models.CharField(max_length=100)
+   email_text = models.EmailField()
+   pw_text = models.CharField(max_length=100)
+   university = models.ForeignKey(Univercity, on_delete=models.CASCADE)
+   faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+   department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+
+class Subscribe(models.Model):
+   user = models.ForeignKey(User, on_delete=models.CASCADE)
+   board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+
+class Board(models.Model):
+   boardname_text = models.CharField(max_lengh=100)
+
+
+class University(models.Model):
+   universityname_text = models.CharField(max_lengh=100)
+   board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+
+class Faculty(models.Model):
+   facultyname_text = models.CharField(max_lengh=100)
+   university = models.ForeignKey(University, on_delete=models.CASCADE)
+   board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+
+class Department(models.Model):
+   departmentname_text = models.CharField(max_lengh=100)
+   faulty = models.ForeignKey(Faulty, on_delete=models.CASCADE)
+   university = models.ForeignKey(University, on_delete=models.CASCADE)
+   board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+
+class Posts(models.Model):
+   user = models.ForeignKey(User, on_delete=models.CASCADE)
+   timestemp = models.DateTimeField(auto_now_add=True)
+   #def was_published_recently(self):
+   #      now = timezone.now()
+   #   return now - datetime.timedelta(days=1) <= self.timestemp <= now
+   title_text = models.CharField(max_length=100)
+   content = models.CharField(max_length=3000)
+   
+
+class Comment(models.Model):
+   user = models.ForeignKey(User, on_delete=models.CASCADE)
+   timestemp = models.DateTimeField(auto_now_add=True)
+      #def was_published_recently(self):
+      #   now = timezone.now()
+      #return now - datetime.timedelta(days=1) <= self.timestemp <= now
+   posts = models.ForeignKey(Posts, on_delete=models.CASCADE)
+   comment_text = models.CharField(max_lengh=1000)
+
+
