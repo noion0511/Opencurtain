0796612f799df757f2fc46402621b5fb5dfa210a
noion <noion0511@gmail.com>
2019-10-29 00:13:21 +0900

    오류수정

--- restserver/views.py
+++ restserver/views.py
@@ -252,13 +252,12 @@ class CommentView(APIView):
         if user == None or user.is_anonymous:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        board = Board.objects.get(pk=request.data.get('board'))
+        posts = Posts.objects.filter(pk=kwargs['post_id'])
         subscribe = Subscribe.objects.filter(board=posts.board, user=user)
 
         if len(subscribe) == 0:
             return Response(status=status.HTTP_404_NOT_FOUND)
         
-        posts = Posts.objects.filter(pk=kwargs['post_id'])
         comment = request.data.get('comment')
         Comment.objects.create(user=user, posts=posts, comment=comment)
         return Response(status=status.HTTP_200_OK)
@@ -272,7 +271,7 @@ class CommentDeleteView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         
         posts = Posts.objects.filter(pk=kwargs['post_id'])
-        comment = Comment.objects.get(pk=request.data.get('comment'))
+        comment = Comment.objects.get(pk=kwargs['comment_id'])
 
         if user == posts.user or user == comment.user:
             comment.delete()
